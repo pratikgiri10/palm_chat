d4df9feb2e0960e732b6b1eb9a21b8d5b5767850
pratikgiri10 <pratikgiri2320@gmail.com>
2025-11-01 19:38:19 +0545

    feat: add user signin

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,30 @@
 import User from "../models/user.model.js"
 import { ApiError } from "../utils/ApiError.js"
+import jwt from 'jsonwebtoken'
+const generateToken = async (userId) => {
+    try {
+        const user = await User.findOne({ _id: userId })
+        const accessToken = jwt.sign({
+            id: user._id,
+            email: user.email
+        }, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        })
+
+        const refreshToken = jwt.sign({
+            id: user._id,
+        },
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            }
+        )
+
+        return { accessToken, refreshToken }
+    } catch (error) {
+        throw new ApiError(error)
+    }
+}
 
 export const register = async (req, res) => {
     const { name, email, password } = req.body
@@ -9,22 +34,57 @@ export const register = async (req, res) => {
 
     }
 
-    const existingUser = await User.findOne({ email })
+    try {
+        const existingUser = await User.findOne({ emai })
 
-    if (existingUser)
-        throw new ApiError(409, 'user already exists')
-    const user = await User.create({
-        name,
-        email,
-        password
-    })
+        if (existingUser)
+            throw new ApiError(409, 'user already exists')
+        const user = await User.create({
+            name,
+            email,
+            password
+        })
 
-    return res.status(201).json({
-        message: 'user created successfully',
-        user
-    })
+        return res.status(201).json({
+            message: 'user created successfully',
+            user
+        })
+
+    } catch (error) {
+        throw new ApiError(500, error)
+    }
 }
 
 export const signin = async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password)
+        throw new ApiError(400, "please fill all the fields")
 
+    try {
+        const user = await User.findOne({ email })
+
+        if (!user)
+            throw new ApiError(404, 'user not found')
+
+        const isMatch = user.comparePassword(password)
+
+        if (!isMatch)
+            throw new ApiError(401, "invalid user credentials")
+        const { accessToken, refreshToken } = generateToken(user._id)
+
+        const options = {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        }
+
+        return res.status(200).cookie("accessToken", accessToken, options).cookie("refreshToken", refreshToken, options).json({
+            user,
+            accessToken,
+            refreshToken,
+            message: 'user logged in successfully'
+        })
+    } catch (error) {
+        throw new ApiError(500, error)
+    }
 }
