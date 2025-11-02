a4cde1f7813b27cc1bb9f58970e3953fad58156b
pratikgiri10 <pratikgiri2320@gmail.com>
2025-11-02 07:28:14 +0545

    feat: server initialization with socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 import 'dotenv/config'
 import app from "./app.js"
 import connectDb from "./config/dbConfig.js"
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: 'http://localhost:5173'
+    }
+})
+
+io.on('connection', async (socket) => {
+    console.log('socket connection established: ', socket.id)
+})
 
 connectDb().then(() => {
     console.log("mongodb connected")
-    app.listen(3000, () => {
+    httpServer.listen(3000, () => {
         console.log('Server is listening at port 3000...')
     })
 }).catch((err) => {
